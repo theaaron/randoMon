b28df862b691d72397a70495e1bee039e4afca16
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-15 21:15:38 -0600

    Initial programmatic setup. Removed Storyboard and created VCs with TabBar.

---
+++ randoMon/ViewControllers/PokemonByTypeVC.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonByTypeVC.swift
+//  randoMon
+//
+//  Created by aaron on 1/15/23.
+//
+
+import UIKit
+
+class PokemonByTypeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
