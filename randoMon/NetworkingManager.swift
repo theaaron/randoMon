febd727ee145817501321df4ffd591a906eff62c
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-15 21:26:43 -0600

    Created Networking manager and PokemonVC for single Pokemon View.

---
+++ randoMon/NetworkingManager.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkingManager.swift
+//  randoMon
+//
+//  Created by aaron on 1/15/23.
+//
+
+import Foundation
+
+struct NetworkingManager {
+    
+    static let shared = NetworkingManager()
+    
+    let allPokemon = "https://pokeapi.co/api/v2/pokemon/"
+    let typePokemon = "https://pokeapi.co/api/v2/type/"
+    
+    
+    func getPokemon(baseUrl: String, completed: @escaping (PokemonByTypeDict?, String?) -> Void) {
+        guard let url = URL(string: baseUrl) else {
+            print("problem with the url")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(nil, "Error: \(String(describing: error))")
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "did not receive 200 status code")
+                return
+            }
+            guard let data = data else {
+                completed(nil, "did not receive data.")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let pokeByTypeDict = try decoder.decode(PokemonByTypeDict.self, from: data)
+                completed(pokeByTypeDict, nil)
+            } catch {
+                completed(nil, "Unable to decode JSON")
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
