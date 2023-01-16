2367585a844edfb25e440f93ac3486fc9361ff6f
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-15 21:16:48 -0600

    Created Pokemon model.

---
+++ Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  randoMon
+//
+//  Created by aaron on 1/15/23.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    let name: String
+    let height: Int
+    let weight: Int
+    let sprites: [Sprites]
+    let types: [Types]
+}
+
+struct Sprites: Codable {
+    let front_default: String
+    let front_shiny: String
+}
+
+struct Types: Codable {
+    let slot: Int
+    let type: Type
+}
+
+struct Type: Codable {
+    let name: String
+    let url: String
+}
