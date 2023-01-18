c020e29709e8226179a69dea765cc93907dcaee8
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-18 00:02:35 -0600

    Added pokemon types to view.

--- randoMon/ViewControllers/PokemonVC.swift
+++ randoMon/ViewControllers/PokemonVC.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class PokemonVC: UIViewController {
     
+    var pokemonObj: Pokemon?
+    var pokemonUrl = ""
     let pokemonNameLabel = UILabel()
     let pokemonImageView = UIImageView()
-    var pokemonObj: Pokemon?
+    let pokemonTypesLabel = UILabel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,7 @@ class PokemonVC: UIViewController {
         DispatchQueue.main.async {
             self.setupNameLabel()
             self.setupPokemonImageView()
+            self.setupPokemonTypesLabel()
         }
         
     }
@@ -27,19 +31,17 @@ class PokemonVC: UIViewController {
     func addSubViews() {
         view.addSubview(pokemonNameLabel)
         view.addSubview(pokemonImageView)
+        view.addSubview(pokemonTypesLabel)
     }
     
     func setupNameLabel() {
-        pokemonNameLabel.text = pokemonObj?.name
+        pokemonNameLabel.text = pokemonObj?.species.name.capitalized
         pokemonNameLabel.textAlignment = .center
         pokemonNameLabel.font = .systemFont(ofSize: 30, weight: .semibold)
         pokemonNameLabel.translatesAutoresizingMaskIntoConstraints = false
         pokemonNameLabel.topAnchor.constraint(equalTo: pokemonImageView.bottomAnchor, constant: 40).isActive = true
         pokemonNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
         pokemonNameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
-        
-        
-        
     }
     
     func setupPokemonImageView() {
@@ -48,13 +50,31 @@ class PokemonVC: UIViewController {
             pokemonImageView.load(url: url)
         }
         
-        
         pokemonImageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
         pokemonImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         pokemonImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
         pokemonImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+    }
+    
+    func setupPokemonTypesLabel() {
+        pokemonTypesLabel.translatesAutoresizingMaskIntoConstraints = false
+        pokemonTypesLabel.text = "Types: \(changeTypesToString(typesArr: pokemonObj?.types ?? []))"
+        pokemonTypesLabel.topAnchor.constraint(equalTo: pokemonNameLabel.bottomAnchor, constant: 10).isActive = true
+        pokemonTypesLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
         
         
+    }
+    
+    func changeTypesToString(typesArr: [Types]) -> String {
+        var typesString = ""
+        for types in typesArr {
+            if typesString.isEmpty {
+                typesString += types.type.name.capitalized
+            } else {
+                typesString += " & \(types.type.name.capitalized)"
+            }
+        }
+        return typesString
     }
     
 
