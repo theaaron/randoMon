6c854d8aa336b09d5803c9555001fdbb9f9d3233
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-17 22:28:03 -0600

    Created PokemonSpeciesDict model to get proper names of pokemon.

---
+++ randoMon/Model/PokemonSpeciesDict.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonSpeciesDict.swift
+//  randoMon
+//
+//  Created by aaron on 1/17/23.
+//
+
+import Foundation
+
+struct PokemonSpeciesDict: Codable {
+    let name: String
+    let url: String
+}
