c394abb2df197d9b3421bfcc0e40432ebd029dbd
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-21 22:03:24 -0600

    Added another model to help with parsing JSON.

--- randoMon/Model/PokemonSpecies.swift
+++ randoMon/Model/PokemonSpecies.swift
@@ -10,9 +10,16 @@ import Foundation
 struct PokemonSpeciesDict: Codable {
     let name: String
     let url: String
+    
 }
 
 struct PokemonSpecies: Codable {
     let name: String
     let id: Int
+    let varieties: [PokemonVariety]
+}
+
+struct PokemonVariety: Codable {
+    let is_default: Bool
+    let pokemon: PokemonBrief
 }
