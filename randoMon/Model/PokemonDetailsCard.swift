e8507b75e6ef9cbf8e0398dc2ca2b87e2de353ef
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-26 23:01:51 -0600

    Created PokemonDetailsCard model to consolidate data from other models.

---
+++ randoMon/Model/PokemonDetailsCard.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonDetailsCard.swift
+//  randoMon
+//
+//  Created by aaron on 1/26/23.
+//
+
+import UIKit
+
+struct PokemonDetailsCard {
+    let name: String
+    let number: Int
+    let types: [String]
+    let image: UIImage
+}
