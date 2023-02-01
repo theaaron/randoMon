fc006d894c80a51589be98babc7a0b71f9526281
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-31 23:58:19 -0600

    Created function to get all pokemon species.

--- randoMon/NetworkingManager.swift
+++ randoMon/NetworkingManager.swift
@@ -11,10 +11,62 @@ struct NetworkingManager {
     
     static let shared = NetworkingManager()
     
-    let allPokemon = "https://pokeapi.co/api/v2/pokemon/"
+    static let allPokemon = "https://pokeapi.co/api/v2/pokemon/"
+    let allPokemonSpecies = "https://pokeapi.co/api/v2/pokemon-species"
     let typePokemon = "https://pokeapi.co/api/v2/type/"
     let genPokemon = "https://pokeapi.co/api/v2/generation/"
     
+    
+    //getting list of pokemon species
+    func getAllPokemonSpecies() async -> AllPokemonSpecies? {
+        guard let url = URL(string: allPokemonSpecies) else {
+            print("error with the url")
+            return nil
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let allPkmn = try JSONDecoder().decode(AllPokemonSpecies.self, from: data)
+            return allPkmn
+        } catch {
+            print("error creating object.")
+            return nil
+        }
+        
+    }
+    
+    
+    //Getting types
+    
+    func getTypes(completed: @escaping (PokemonTypesDict?, String?) -> Void) {
+        guard let url = URL(string: typePokemon) else {
+            print("problem with the url")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(nil, String(describing: error))
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "not 200")
+                return
+            }
+            guard let data = data else {
+                completed(nil, "there was no data")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedTypes = try decoder.decode(PokemonTypesDict.self, from: data)
+                completed(decodedTypes, nil)
+            } catch {
+                completed(nil, "unable to decode the JSON")
+            }
+        }
+        task.resume()
+    }
     
     func getPokemonFromTypesList(baseUrl: String, completed: @escaping (PokemonByTypeDict?, String?) -> Void) {
         guard let url = URL(string: baseUrl) else {
@@ -61,6 +113,8 @@ struct NetworkingManager {
         }
     }
     
+    //Getting gens and pokemon by gens
+    
     func getAllPokemonGens() async -> [GenerationResults]? {
         let url = URL(string: genPokemon)
         guard let url = url else {
@@ -78,7 +132,7 @@ struct NetworkingManager {
         
     }
     
-    func getPokemonByGen(genNumber: String) async -> [PokemonSpeciesDict] {
+    func getPokemonByGen(genNumber: String) async -> [PokemonSpeciesUrl] {
         let url = URL(string: genPokemon + genNumber)
         guard let url = url else {
             print("invalid url")
@@ -94,7 +148,7 @@ struct NetworkingManager {
         }
     }
     
-    
+    // getting Pokemon objects
     
     func getPokemonObj(pkmnUrl: String) async -> Pokemon? {
         let url = URL(string: pkmnUrl)
@@ -127,71 +181,7 @@ struct NetworkingManager {
         }
     }
     
-    
-    func getTypes(completed: @escaping (PokemonTypesDict?, String?) -> Void) {
-        guard let url = URL(string: typePokemon) else {
-            print("problem with the url")
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let _ = error {
-                completed(nil, String(describing: error))
-            }
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "not 200")
-                return
-            }
-            guard let data = data else {
-                completed(nil, "there was no data")
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let decodedTypes = try decoder.decode(PokemonTypesDict.self, from: data)
-                completed(decodedTypes, nil)
-            } catch {
-                completed(nil, "unable to decode the JSON")
-            }
-        }
-        task.resume()
-    }
-    
-    
-    
-    func getPokemonObject(pokemonUrl: String, completed: @escaping (Pokemon?, String?) -> Void) {
-        guard let url = URL(string: pokemonUrl) else {
-            print(pokemonUrl)
-            print("could not make url")
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let _ = error {
-                completed(nil, String(describing: error))
-            }
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "did not receive 200")
-                return
-            }
-            
-            guard let data = data else {
-                completed(nil, "no data")
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let decodedPokemon = try decoder.decode(Pokemon.self, from: data)
-                completed(decodedPokemon, nil)
-            } catch {
-                return
-            }
-            
-        }
-        task.resume()
-    }
+    //getting properties for creating a card.
     
     func getPokemonDetailCard(pkmnUrl: String, pkmnSpecUrl: String) async -> PokemonDetailsCard {
         let pokemonObj = await getPokemonObj(pkmnUrl: pkmnUrl)
@@ -202,7 +192,8 @@ struct NetworkingManager {
                                   frontSprite: pokemonObj?.sprites.front_default ?? "",
                                   shinyFrontSprite: pokemonObj?.sprites.front_shiny ?? "", flavorText: pokemonSpecies?.flavor_text_entries.filter({ flavorText in
             flavorText.language.name == "en"
-        }) ?? [])
+        }) ?? [],
+                                  varieties: pokemonSpecies?.varieties ?? [])
     }
     
     
