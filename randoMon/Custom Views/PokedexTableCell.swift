deb372eb4a027ca7477d181cff6fac94704e0091
aaron <4142467+theaaron@users.noreply.github.com>
2023-01-31 23:57:32 -0600

    Styled cell for pokedex.

--- randoMon/Custom Views/PokedexTableCell.swift
+++ randoMon/Custom Views/PokedexTableCell.swift
@@ -8,7 +8,57 @@
 import UIKit
 
 class PokedexTableCell: UITableViewCell {
-
+    static let reuseId = "DexCell"
+    
+    let pokeImageView = UIImageView()
+    let pokeNameLabel = UILabel()
+    
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        setupCell()
+//    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+    
+    func set(pokeSpeciesUrl: PokemonSpeciesUrl) {
+        Task {
+            let pokeSpecies = await NetworkingManager.shared.getPokemonSpecies(url: pokeSpeciesUrl.url)
+            let pokeObjUrl = NetworkingManager.allPokemon + String(pokeSpecies?.id ?? 1)
+            let pokeObj = await NetworkingManager.shared.getPokemonObj(pkmnUrl: pokeObjUrl)
+            let imageURL: URL = URL(string: pokeObj?.sprites.front_shiny ?? "")!
+            pokeImageView.load(url: imageURL)
+            pokeNameLabel.text = pokeSpecies?.name.capitalized
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCell() {
+        addSubview(pokeImageView)
+        addSubview(pokeNameLabel)
+        
+        //setup image
+        pokeImageView.translatesAutoresizingMaskIntoConstraints = false
+        pokeImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        pokeImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        pokeImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        pokeImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        //setup label
+        pokeNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        pokeNameLabel.leadingAnchor.constraint(equalTo: pokeImageView.trailingAnchor, constant: 20).isActive = true
+        pokeNameLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+    }
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
